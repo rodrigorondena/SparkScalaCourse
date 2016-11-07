c47dab53b789f1f2805b88465df0b618692f2e56
rodrigorondena <rodrigorondena@gmail.com>
2016-11-07 11:43:04 -0200

    Finish Session 3

---
+++ src/com/sundogsoftware/spark/PurchaseByCustomer.scala
@@ -0,0 +1,34 @@
+package com.sundogsoftware.spark
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+
+object PurchaseByCustomer {
+   /** Convert input data to (customerID, amountSpent) tuples */
+  def extractCustomerPricePairs(line: String) = {
+    val fields = line.split(",")
+    (fields(0).toInt, fields(2).toFloat)
+  }
+ 
+  /** Our main function where the action happens */
+  def main(args: Array[String]) {
+   
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+    
+     // Create a SparkContext using every core of the local machine
+    val sc = new SparkContext("local[*]", "TotalSpentByCustomer")   
+    
+    val input = sc.textFile("C:/Users/rodrigo.rondena/Desktop/SparkScala/SparkScalaCourse/customer-orders.csv")
+
+    val mappedInput = input.map(extractCustomerPricePairs)
+    
+    val totalByCustomer = mappedInput.reduceByKey( (x,y) => x + y )
+    
+    val results = totalByCustomer.collect()
+    
+    // Print the results.
+    results.foreach(println)
+  }
+}
